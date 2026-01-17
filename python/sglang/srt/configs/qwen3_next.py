a5d95472f851ebf705277a9ffd2e1671c2d31634
luotianwei <2685287173@qq.com>
2026-01-17 23:00:36 +0800

    feat: implement qwen3-kimi attention_type choosing according to different layer_type config

--- python/sglang/srt/configs/qwen3_next.py
+++ python/sglang/srt/configs/qwen3_next.py
@@ -248,6 +248,8 @@ class Qwen3NextConfig(PretrainedConfig):
         
         full_interval = getattr(self, "full_attention_interval", None)
         linear_interval = getattr(self, "linear_attention_interval", None)
+        
+        self.layer_types=layer_types
 
         if full_interval is None and linear_interval is not None:
             self.full_attention_interval = linear_interval
@@ -255,6 +257,17 @@ class Qwen3NextConfig(PretrainedConfig):
     @property
     def layers_block_type(self):
         layer_type_list = []
+        
+        if self.layer_types is not None:
+            for l in range(self.num_hidden_layers):
+                if self.layer_types[l]=='full_attention':
+                    layer_type_list.append(HybridLayerType.full_attention.value)
+                elif self.layer_types[l]=='linear_attention':
+                    layer_type_list.append(HybridLayerType.linear_attention.value)
+                else:
+                    raise NotImplementedError(f'Unsupported layer type: {self.layer_types[l]}')
+            return layer_type_list
+
 
         for l in range(self.num_hidden_layers):
             if (l + 1) % self.full_attention_interval == 0:
